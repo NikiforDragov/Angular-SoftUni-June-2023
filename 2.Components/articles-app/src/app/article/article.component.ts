e2d55c3e895f60bd6233fda491d376c0a67d7e89
Nikifor Dragov <ndragov99@gmail.com>
2023-07-03 20:05:46 +0300

    create needed properties

--- 2.Components/articles-app/src/app/article/article.component.ts
+++ 2.Components/articles-app/src/app/article/article.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+  styleUrls: ['./article.component.css'],
 })
 export class ArticleComponent {
+  constructor() {
+    this.articleDescLen = 0;
+    this.descToShow = '';
+  }
+  private symbols: number = 250;
+  @Input() article: Article;
+  @Input() articleDesc: string;
 
+  descToShow: string;
+  articleDescLen: number;
+  showReadMoreBtn: boolean = true;
+  showHideBtn: boolean = false;
+  imageIsShown: boolean = false;
+  imageButtonTitle: string = 'Show Image';
 }
