c051b5f1186a3309c9085d0437f18e3c41c35eea
Nikifor Dragov <ndragov99@gmail.com>
2023-07-09 18:23:41 +0300

    Add welcome component

--- Workshop Modules and Routing/modules-and-routing-workshop/src/app/user/login/login.component.ts
+++ Workshop Modules and Routing/modules-and-routing-workshop/src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { UserService } from '../user.service';
 
 @Component({
@@ -7,11 +8,12 @@ import { UserService } from '../user.service';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  constructor(private UserService: UserService) {}
+  constructor(private UserService: UserService, private router: Router) {}
 
   login(email: string, password: string): void {
     //For now we are not handling the data
 
     this.UserService.login();
+    this.router.navigate(['/']);
   }
 }
